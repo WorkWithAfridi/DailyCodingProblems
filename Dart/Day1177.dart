89fe99135e887d2398c7fecb53fc664e4c2dce4b
khondakar.afridi <afridi.khondakar@gmail.com>
2024-05-06 10:32:51 +0600

    Day 1177 added

---
+++ Dart/Day1177.dart
@@ -0,0 +1,29 @@
+// Given an array of integers where every integer occurs three times except for one integer,
+// which only occurs once, find and return the non-duplicated integer.
+
+// For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+
+void main(List<String> args) {
+  List<int> myList = [6, 1, 3, 3, 3, 6, 6];
+  int? result = findSingle(myList);
+  if (result != null) {
+    print("Single occurance item is : " + result.toString());
+  } else {
+    print("No single occurance item found");
+  }
+}
+
+int? findSingle(List<int> myList) {
+  Map<int, int> occuranceCountMap = {};
+  for (int num in myList) {
+    occuranceCountMap[num] = (occuranceCountMap[num] ?? 0) + 1;
+  }
+
+  for (int num in myList) {
+    if (occuranceCountMap[num] == 1) {
+      return num;
+    }
+  }
+
+  return null;
+}
